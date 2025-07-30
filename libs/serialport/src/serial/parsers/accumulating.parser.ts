632b4d8e1f66588aa3d5a5d47e0e076de3852f60
The Van <thevan@evol.vn>
2025-07-31 02:54:22 +0700

    fix(libs): serial-port handle msg

--- libs/serialport/src/serial/parsers/accumulating.parser.ts
+++ libs/serialport/src/serial/parsers/accumulating.parser.ts
@@ -1,39 +1,58 @@
+/* eslint-disable */
 import { Transform } from 'stream';
 
-export class AccumulatingParser extends Transform {
-  private _buffer: Buffer = Buffer.alloc(0);
-  private readonly _maxSize: number;
-  private readonly _delimiter?: Buffer;
+class AccumulatingParser extends Transform {
+  private buffer: Buffer = Buffer.alloc(0);
+  private maxSize: number;
+  private delimiter?: Buffer;
 
   constructor(options: { maxSize?: number; delimiter?: string } = {}) {
-    super();
-    this._maxSize = options.maxSize || 1024 * 1024;
-    this._delimiter = options.delimiter ? Buffer.from(options.delimiter) : undefined;
+    super({ readableObjectMode: true }); // Important for emitting strings
+    this.maxSize = options.maxSize || 1024 * 1024;
+    this.delimiter = options.delimiter ? Buffer.from(options.delimiter) : undefined;
   }
 
-  public _transform(chunk: Buffer, encoding: string, callback: () => void): void {
-    this._buffer = Buffer.concat([this._buffer, chunk]);
+  _transform(chunk: Buffer, encoding: string, callback: () => void) {
+    this.buffer = Buffer.concat([this.buffer, chunk]);
 
-    if (this._buffer.length >= this._maxSize) {
-      this.push(this._buffer);
-      this._buffer = Buffer.alloc(0);
-    }
-
-    if (this._delimiter) {
-      const delimiterIndex = this._buffer.indexOf(this._delimiter);
-      if (delimiterIndex !== -1) {
-        const message = this._buffer.slice(0, delimiterIndex);
-        this.push(message);
-        this._buffer = this._buffer.slice(delimiterIndex + this._delimiter.length);
-      }
+    if (this.checkEmitCondition()) {
+      this.emitData();
     }
 
     callback();
   }
 
-  public _flush(callback: () => void): void {
-    if (this._buffer.length > 0) {
-      this.push(this._buffer);
+  private checkEmitCondition(): boolean {
+    if (this.buffer.length >= this.maxSize) {
+      return true;
+    }
+
+    if (this.delimiter) {
+      return this.buffer.indexOf(this.delimiter) !== -1;
+    }
+
+    return true;
+  }
+
+  private emitData() {
+    if (this.delimiter) {
+      let delimiterIndex;
+      while ((delimiterIndex = this.buffer.indexOf(this.delimiter)) !== -1) {
+        const message = this.buffer.slice(0, delimiterIndex);
+        this.emit('data', message.toString()); // Emit as string
+        this.buffer = this.buffer.slice(delimiterIndex + this.delimiter.length);
+      }
+    } else {
+      this.emit('data', this.buffer.toString());
+      if (this.buffer.length >= this.maxSize) {
+        this.buffer = Buffer.alloc(0);
+      }
+    }
+  }
+
+  _flush(callback: () => void) {
+    if (this.buffer.length > 0) {
+      this.emit('data', this.buffer.toString());
     }
     callback();
   }
